85d90e376ed1ebfdf0e7e7e8ef5366d71ceb3e6f
garypaulchamberlain <garypchamberlain@gmail.com>
2024-03-22 10:12:45 +0000

    Updated fonts to use webfonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,4 +3,7 @@ import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Lusitana({subsets: ['latin'],weight:"400"});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
